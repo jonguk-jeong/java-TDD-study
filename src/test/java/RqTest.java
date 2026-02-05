8315a93662c9f4048a84f2170ae9f6d9f02afe50
jonguk-jeong <whddnr112233@gmail.com>
2026-02-05 14:24:29 +0900

    T6 - b

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -57,6 +57,14 @@ public class RqTest {
         int rst = rq.getParamAsInt("page");
         assertThat(rst).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName(value = "명령: 목록?page=10 일때, rq.getParamAsInt(\"page\") -> 10")
+    void t7() {
+        Rq rq = new Rq("목록?page=10");
+        int rst = rq.getParamAsInt("page");
+        assertThat(rst).isEqualTo(10);
+    }
 }
 
 // displayName을 잘 적어주는게 좋다
