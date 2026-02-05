72d8de7d54ff844dc6b7c885f1abcb9683ca4b1c
jonguk-jeong <whddnr112233@gmail.com>
2026-02-05 14:30:54 +0900

    t6 - b 이게 b

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -41,13 +41,7 @@ public class Rq {
     }
 
     public int getParamAsInt(String key) {
-        if(cmd.equals("목록?page=1") && key.equals("page")) {
-            return 1;
-        }
-        if(cmd.equals("목록?page=10") && key.equals("page")) {
-            return 10;
-        }
-
-        return 0;
+        String rst = getParam(key);
+        return Integer.parseInt(rst);
     }
 }
